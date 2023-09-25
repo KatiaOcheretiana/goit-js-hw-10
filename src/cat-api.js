cce5cf1e135503bd57cbaefb0b083468f085cba7
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-09-25 11:10:10 +0300

    update script

--- src/cat-api.js
+++ src/cat-api.js
@@ -7,60 +7,25 @@ import {showLoader, hideLoader, hideSelect, showSelect, hideCatInfo, showCatInfo
 export function fetchBreeds() {
     const BASE_URL = "https://api.thecatapi.com/v1/breeds"
 
-hideSelect()
-showLoader()
-
-
-    axios.get(BASE_URL)
+   return axios.get(BASE_URL)
         .then(function (response) {
-            const data = response.data;
-
-    const result = data.map(item => {
-        return `<option value=${item.id}>${item.name}</option>`
-    })
-          selectors.breedSelect.innerHTML = result.join('')
-          
+          const data = response.data;
+   return data
 })
         .catch(function (error) {
  showError()
-         
-    console.log(error);
   })
-    .finally(function () {
-hideLoader();
-      showSelect()
-  });  
+     
 }
-
 
 export function fetchCatByBreed(breedId) {
     const BASE_URL = "https://api.thecatapi.com/v1/images/search"
-
-hideCatInfo()
-showLoader()
-
-    selectors.catInfo.innerHTML = "";
-
-     axios.get(`${BASE_URL}?breed_ids=${breedId}`)
+     return axios.get(`${BASE_URL}?breed_ids=${breedId}`)
         .then(function (response) {
             const catData = response.data[0];
-
-    const result = `<div class="cat-img"><img src=${catData.url} alt=${catData.breeds[0].name}/></div>
-                 <div class="text-content"><h1 class="cat-name">${catData.breeds[0].name}</h1>
-                 <p>${catData.breeds[0].description}</p>
-                 <p><span class="cat-temperament">Temperament: </span>${catData.breeds[0].temperament
-}</p></div>`
-          selectors.catInfo.innerHTML = result
-            
+            return catData
          })
          .catch(function (error) {
 showError()
-    console.log(error);
   })
-    .finally(function () {
-hideLoader();
-showCatInfo()
-  });  
 }
-
-
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { selectors } from "./selectors";
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-import SlimSelect from 'slim-select'
-
+import {showLoader, hideLoader, hideSelect, showSelect, hideCatInfo, showCatInfo, showError} from "./show-hide-el.js";
 
 
 const API_KEY = "live_iOrcxGPLNh9qtia2MFXzkQ97Pfy4zCdnyp5ETGT9VccjJnn1hQ9aiRiVSIRvKFLi"
@@ -10,14 +9,43 @@ axios.defaults.headers.common["x-api-key"] = API_KEY;
 
 
 
+fetchBreeds().then (data => {
+    hideSelect()
+    showLoader()
+    const result = data.map(item => {
+        return `<option value=${item.id}>${item.name}</option>`
+    })
+    selectors.breedSelect.innerHTML = result.join('')
+      
+})
+.finally(function () {
+hideLoader();
+      showSelect()
+  }); 
 
-fetchBreeds()
 
 selectors.breedSelect.addEventListener('change', onChangeBreed)
 
 
 function onChangeBreed(event) {
+         selectors.catInfo.innerHTML = "";
+            hideCatInfo();
+            showLoader();
     const selectedCat = event.currentTarget.value
-    fetchCatByBreed(selectedCat)
+    fetchCatByBreed(selectedCat).then(catData => {
+    createMarkup(catData)}  
+    )
+.finally(function () {
+hideLoader();
+showCatInfo()
+  });  
 }
 
+function createMarkup(catData) {
+         const result = `<div class="cat-img"><img src=${catData.url} alt=${catData.breeds[0].name}/></div>
+                 <div class="text-content"><h1 class="cat-name">${catData.breeds[0].name}</h1>
+                 <p>${catData.breeds[0].description}</p>
+                 <p><span class="cat-temperament">Temperament: </span>${catData.breeds[0].temperament
+}</p></div>`
+    selectors.catInfo.innerHTML = result
+}
